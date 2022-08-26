ae7fe324f2189f80d33a187d28b46f3fefb936b4
MarVikt <v.m.podporogie@gmail.com>
2022-08-26 14:05:31 +0300

    added message about send

--- src/modules/sendForm.js
+++ src/modules/sendForm.js
@@ -1,6 +1,7 @@
 const sendForm = (formId, formBody) => {
   const dataBaseUrl = 'https://jsonplaceholder.typicode.com/posts';
   const form = document.getElementById(formId);
+  const itWasSent = document.querySelector('.it-was-sent');
 
   const sendData = (data) => {
     return fetch(dataBaseUrl, {
@@ -41,11 +42,24 @@ const sendForm = (formId, formBody) => {
       form.append(statusBlock);
     }
     statusBlock.textContent = 'Отправка...';
+    itWasSent.style.display = 'none';
+
     sendData(formBody)
       .then(data => {
         statusBlock.textContent = 'Спасибо! Наш менеджер свяжется с Вами!';
-        console.log(data.id);
         deleteBlock(statusBlock,3000);
+
+        // отобразим отправленные сведения где-то справа
+        itWasSent.style.display = 'block';
+        itWasSent.innerHTML = '';
+        itWasSent.insertAdjacentHTML('beforeend',`
+        <p>Заявка отправлена:</p>
+        `);
+        for (let key in data) {
+          itWasSent.insertAdjacentHTML('beforeend',`
+          <div><b>${key}:</b> ${data[key]}</div>
+          `);
+        }
       })
       .catch(error => {
         console.log(error.message);
